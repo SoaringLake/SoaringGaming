92cd334293825cbaad03eb751e40d5ef99e30e40
SoaringLake <39405326+SoaringLake@users.noreply.github.com>
2018-05-18 12:45:59 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+	<head>
+		<title>SoaringGaming - Najczystszy serwer minecraft survival w polsce</title>
+		<link rel="Stylesheet" type="text/css" href="style.css" />
+	</head>
+	<body>
+		<div id="page">
+			<div id="menu">
+				<button id="active">Strona główna </button>
+				<button><Regulamin </button>
+				<button>Administracja</button>
+				<button>Panel gracza</button>
+				<button>Mapa świata</button>
+			</div>
+			<div id="center">
+			<h2>Dlaczego my?</h2><br>
+			To proste, nasz serwer jest jednym z najczystszych (pozbawionym niepotrzebnych pluginów mogących powodować lagi) w Polsce, każdy na naszym serwerze jest równy bez względu na to czy ktoś skorzysta z opcji w Sklepie czy nie<br>
+			<h2>Jak z atmosferą na serwerze?</h2><br>
+			U nas zawsze panuje miła atmosfera, jeżeli ktoś by zaczął obrażać innych graczy lub robić coś nieprzyzwoitego w jaki kolwiek sposób zostanie automatycznie wyrzucony z serwera.<br>
+			<h2>Co potrzebuje żeby u was zagrać?</h2>
+			Potrzebujesz Minecraft (Premium lub non-premium) w wersji 1.12.X oraz połączenie z internetem oraz adres IP serwera który jest taki sam jak adres strony na której jesteś
+			</div>
+			<div id="info o serwerze">
+			Serwer
+			
+			
+			</div>
+		</div>
+	</body>
+</html>
